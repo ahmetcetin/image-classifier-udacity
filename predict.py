9408ca601702fc7fd80521825877c6ea2094a582
Ahmet Cetin <ahmet@cetin.info>
2018-07-20 18:00:36 +0200

    part2 completed

--- predict.py
+++ predict.py
@@ -1,1 +1,166 @@
 #!/usr/bin/env python3
+import argparse
+import os
+import sys
+from pathlib import Path
+import matplotlib.pyplot as plt
+import json
+import math
+from PIL import Image
+from collections import OrderedDict
+
+import torch
+import numpy as np
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms, models
+
+from utils import query_yes_no
+from pick import pick
+
+parser = argparse.ArgumentParser(
+    description='This is trainer program.',
+)
+
+parser.add_argument('image_file')
+parser.add_argument('--top_k', action='store', default=3, type=int)
+parser.add_argument('--models_dir', action='store', default='.')
+parser.add_argument('--category_names', action='store', default='cat_to_name.json')
+parser.add_argument('--gpu', action='store_true', default=False)
+
+args = parser.parse_args()
+
+files = os.listdir(args.models_dir)
+trained_models = [i for i in files if i.endswith('.mdl')]
+
+if len(trained_models) == 0:
+    print('Error: There is no pretrained model in models directory - ', args.models_dir)
+    print('Please change the models directory using --models_dir option')
+    exit(1)
+
+title = 'Please choose pretrained model to use: '
+model_file, _ = pick(trained_models, title)
+
+with open(args.category_names, 'r') as f:
+    cat_to_name = json.load(f)
+
+check_gpu = 'cuda:0' if args.gpu and torch.cuda.is_available() else 'cpu'
+
+if args.gpu and check_gpu == 'cpu':
+    confirm_cpu = query_yes_no('There is no cuda compatible gpu exists, do you want to continue using cpu?')
+    if not confirm_cpu:
+        exit(0)
+
+print('Running prediction, please wait...')
+
+device = torch.device(check_gpu)
+
+state = torch.load(model_file, map_location=check_gpu)
+
+
+model = getattr(models, state['arch'])(pretrained=True)
+for param in model.parameters():
+    param.requires_grad = False
+
+
+if 'vgg' in state['arch']:
+    first_layer_input = 25088
+elif 'densenet' in state['arch']:
+    first_layer_input = 1024
+
+layers = []
+prev_layer = first_layer_input
+
+for i, layer in enumerate(state['hidden_units']):
+    layers.append(('class_layer_' + str(i + 1), nn.Linear(prev_layer, layer)))
+    layers.append(('relu_' + str(i + 1), nn.ReLU()))
+    prev_layer = layer
+
+layers.append(('fc_last', nn.Linear(prev_layer, 102)))
+layers.append(('output', nn.LogSoftmax(dim=1)))
+
+classifier = nn.Sequential(OrderedDict(layers))
+model.classifier = classifier
+
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=state['learning_rate'])
+
+model.load_state_dict(state['state_dict'])
+optimizer.load_state_dict(state['optimizer'])
+
+class_to_idx = state['class_to_idx']
+class_to_idx = {str(x): str(y) for x, y in class_to_idx.items()}
+idx_to_class = {y: x for x, y in class_to_idx.items()}
+
+
+def process_image(image_file):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    img = Image.open(image_file)
+    width, height = img.size
+    ratio = height / width
+    shortsize = 256
+
+    if height > width:
+        longsize = int(math.floor(shortsize * ratio))
+        newsize = (shortsize, longsize)
+    else:
+        longsize = int(math.floor(shortsize / ratio))
+        newsize = (longsize, shortsize)
+
+    img = img.resize(newsize)
+
+    w, h = newsize
+
+    left = (w - 224)/2
+    top = (h - 224)/2
+    right = (w + 224)/2
+    bottom = (h + 224)/2
+
+    cropped = img.crop((left, top, right, bottom))
+
+    imgplot = plt.imshow(cropped)
+    imgplot.axes.get_xaxis().set_visible(False)
+    imgplot.axes.get_yaxis().set_visible(False)
+
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+
+    image_file = np.array(cropped) / 255
+    image_file = (image_file - mean) / std
+    image_file = image_file.transpose((2, 0, 1))
+
+    return torch.from_numpy(image_file)
+
+
+# image = process_image('flowers/test/28/image_05230.jpg')
+def predict(image_path, model, topk=5):
+    """ Predict the class (or classes) of an image using a trained deep learning model.
+    """
+    model.eval()
+    model = model.to(device)
+    image = process_image(image_path)
+    image = torch.unsqueeze(image, 0)
+    image = image.to(device).float()
+    result = model(Variable(image))
+    result = F.softmax(result, dim=1)
+    result = result.cpu()
+    p, c = result.topk(topk)
+    p, c = p.detach().numpy()[0], c.detach().numpy()[0]
+    return p, c
+
+
+def get_class_name(idx):
+    return cat_to_name[idx_to_class[str(idx)]]
+
+
+probs, classes = predict(args.image_file, model, args.top_k)
+class_names = [get_class_name(x) for x in classes]
+
+print('Most probably: ', get_class_name(classes[0]))
+print('\nProbabilities:')
+for a, b in zip(class_names, probs):
+    print("{}: {:.4%}".format(a, b))
--- train.py
+++ train.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 import argparse
 import os
-from pathlib import Path
-import matplotlib.pyplot as plt
-import json
-import math
-from PIL import Image
 from collections import OrderedDict
 
 import torch
-import numpy as np
 from torch import nn
 from torch import optim
-import torch.nn.functional as F
-from torch.autograd import Variable
 from torchvision import datasets, transforms, models
 
 from utils import query_yes_no
@@ -22,16 +14,18 @@ parser = argparse.ArgumentParser(
     description='This is trainer program.',
 )
 
-parser.add_argument('data_directory', default='flowers')
+parser.add_argument('data_directory')
 parser.add_argument('--save_dir', action='store', default='.')
 parser.add_argument('--arch', action='store', default='densenet121')
 parser.add_argument('--learning_rate', action='store', default=.01, type=float)
 parser.add_argument('--epochs', action='store', default=40, type=int)
 parser.add_argument('--gpu', action='store_true', default=False)
-parser.add_argument('--hidden_units', action='store', default=512)
+parser.add_argument('--hidden_units', action='append', default=[], type=int)
 
 args = parser.parse_args()
 
+if len(args.hidden_units) == 0:
+    args.hidden_units = [512]
 
 try:
     if not os.path.exists(args.save_dir):
@@ -41,11 +35,10 @@ except OSError:
     exit(1)
 
 model_file = args.save_dir + '/' + args.arch + '.mdl'
-print(model_file)
 
 if os.path.isfile(model_file):
-    answer = query_yes_no('model already exists on save dir, do you want to overwrite?')
-    if not answer:
+    overwrite = query_yes_no('model already exists on save dir, do you want to overwrite?')
+    if not overwrite:
         exit(0)
 
 train_dir = args.data_directory + '/train'
@@ -96,3 +89,162 @@ dataloaders = {
     "test": torch.utils.data.DataLoader(image_datasets["test"], batch_size=32)
 }
 
+
+def get_pretrained_model(name):
+    conv_models = {'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn', 'densenet121', 'densenet169', 'densenet161', 'densenet201'}
+    if name not in conv_models:
+        print('Error: Sorry, there is no such pretrained model. ' + name)
+        print('You can use one of the following predefined models:')
+        print(str(conv_models))
+        exit(1)
+
+    return getattr(models, name)
+
+
+init_lr = args.learning_rate
+epochs = args.epochs
+
+model = get_pretrained_model(args.arch)(pretrained=True)
+for param in model.parameters():
+    param.requires_grad = False
+
+
+if 'vgg' in args.arch:
+    first_layer_input = 25088
+elif 'densenet' in args.arch:
+    first_layer_input = 1024
+
+layers = []
+prev_layer = first_layer_input
+
+for i, layer in enumerate(args.hidden_units):
+    layers.append(('class_layer_' + str(i + 1), nn.Linear(prev_layer, layer)))
+    layers.append(('relu_' + str(i + 1), nn.ReLU()))
+    prev_layer = layer
+
+layers.append(('fc_last', nn.Linear(prev_layer, 102)))
+layers.append(('output', nn.LogSoftmax(dim=1)))
+
+classifier = nn.Sequential(OrderedDict(layers))
+model.classifier = classifier
+
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=init_lr)
+
+
+check_gpu = 'cuda:0' if args.gpu and torch.cuda.is_available() else 'cpu'
+
+if args.gpu and check_gpu == 'cpu':
+    confirm_cpu = query_yes_no('There is no cuda compatible gpu exists, do you want to continue using cpu?')
+    if not confirm_cpu:
+        exit(0)
+
+device = torch.device(check_gpu)
+model.to(device)
+
+print_every = 50
+steps = 0
+
+
+def adjust_lr(optimizer, epoch):
+    lr = init_lr * (0.1 ** (epoch // 20))
+    print('Starting epoch {}, learning rate {}'.format(epoch+1, lr))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def validation(model, validationloader, criterion):
+    test_loss = 0
+    accuracy = 0
+    for inputs, labels in validationloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        output = model.forward(inputs)
+        test_loss += criterion(output, labels).item()
+
+        ps = torch.exp(output)
+        equality = (labels.data == ps.max(dim=1)[1])
+        accuracy += equality.type(torch.FloatTensor).mean()
+
+    return test_loss, accuracy
+
+print('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*')
+print('Epochs: ', args.epochs)
+print('Starting learning rate: ', args.learning_rate)
+print('Pre-trained architecture: ', args.arch)
+print('Classification layers: ', args.hidden_units)
+print('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*')
+
+confirm_start = query_yes_no('All checks good, do you want to start training with above parameters?')
+
+if not confirm_start:
+    exit(0)
+
+for e in range(epochs):
+    running_loss = 0
+    adjust_lr(optimizer, e)
+    for inputs, labels in dataloaders['training']:
+        steps += 1
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+
+        outputs = model.forward(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if steps % print_every == 0:
+            model.eval()
+
+            with torch.no_grad():
+                test_loss, accuracy = validation(model, dataloaders['validation'], criterion)
+
+            print("Epoch: {}/{}... ".format(e+1, epochs),
+                  "Training Loss: {:.3f} ".format(running_loss/print_every),
+                  "Validation Loss: {:.3f} ".format(test_loss/len(dataloaders['validation'])),
+                  "Validation Accuracy: {:.3f}".format(accuracy/len(dataloaders['validation'])))
+
+            running_loss = 0
+
+            model.train()
+
+correct = 0
+total = 0
+model = model.to(device)
+model.eval()
+
+with torch.no_grad():
+    for data in dataloaders['test']:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+confirm_save = query_yes_no('Are you happy with the test results? Do you want to save the model?')
+
+if not confirm_save:
+    exit(0)
+
+model.class_to_idx = image_datasets['training'].class_to_idx
+
+
+def save_checkpoint():
+    state = {
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'class_to_idx': model.class_to_idx,
+        'epochs': args.epochs,
+        'learning_rate': args.learning_rate,
+        'hidden_units': args.hidden_units,
+        'arch': args.arch
+    }
+
+    torch.save(state, model_file)
+
+
+save_checkpoint()
